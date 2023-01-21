0e82161bab14a3b7e2c0ce51f5be2f4e635e98ba
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-01-21 07:53:51 +0300

    fix signin

--- routes/users.js
+++ routes/users.js
@@ -46,17 +46,17 @@ const getUserData = async function (req, res){
     res.json(user);
 }
 
-router.post('/signin', createBodySchemaParser(User),createRouteCallback(signin));
+router.post('/signin', createBodySchemaParser(User, 'createSchemaDto'),createRouteCallback(signin));
 router.post('/login', createBodySchemaParser(User, 'loginSchemaDto'), createRouteCallback(login));
 router.get('/logout',createAuth(), createRouteCallback(logout));
 router.post('/can-access',createAuth(), createBodySchemaParser(User, 'canAccessDto'), createRouteCallback(canAccess));
 router.get('/user-data',createAuth(), createRouteCallback(getUserData));
-
+/*
 const createTest  = (n) => function (req, res){
     res.json({message:"Called test "+n})
 }
 router.get('/test-1', createAuth(), createTest(1))
 router.get('/test-2', createAuth([1]), createTest(2))
 router.get('/test-3', createAuth([2]), createTest(3))
-
+*/
 module.exports = router;
