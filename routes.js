87c0f0806197abe1489ea7c2b375c72a2e931494
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-01-22 15:58:38 +0300

    basic historic

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 
 const carsRoutes = require('./routes/cars')
+const repairsHistoricRoutes = require('./routes/repairs-historic')
 const rapairsRoutes = require('./routes/repairs')
 const expensesRoutes = require('./routes/expenses')
 const usersRoutes = require('./routes/users')
 
 module.exports = function (app){
     app.use("/cars", carsRoutes);
+    app.use("/repairs-historic", repairsHistoricRoutes);
     app.use("/repairs", rapairsRoutes);
     app.use("/expenses", expensesRoutes);
     app.use("/users", usersRoutes);
