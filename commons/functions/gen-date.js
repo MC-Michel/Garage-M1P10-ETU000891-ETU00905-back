bf3ecc74cccc423df557bacbf56a9e1ae0723f13
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-01-23 20:42:16 +0100

    add current repairs stats fetch

--- commons/functions/gen-date.js
+++ commons/functions/gen-date.js
@@ -3,3 +3,19 @@ module.exports.addDays = function (date, days){
     ans.setDate(date.getDate()+days);
     return ans;
 }
+
+module.exports.generateMonths = function (year){
+
+}
+
+module.exports.generateDays = function (month){
+
+}
+
+module.exports.formatAndTrunc = function (date, truncTo){
+    const year = date.getYear();
+    const month = date.getMonth()+1
+    let ans = year.toString();
+    if(truncTo === 'year') return ans;
+    return ans+'-'+month.toString().padStart(2,'0');
+}
--- repositories/car.repo.js
+++ repositories/car.repo.js
@@ -1,18 +1,67 @@
 const GenRepository = require("../commons/database/class/gen-repository");
+const { formatAndTrunc } = require("../commons/functions/gen-date");
 const { getConnection } = require("../configs/db");
 const Car = require("../models/car.model");
-
+const vars = {
+    "year": {
+        timePeriodFormat: "%Y-%m",
+        timePeriodFormatForFilter: "%Y",
+    },
+    "month": {
+        timePeriodFormat: "%Y-%m-%d",
+        timePeriodFormatForFilter: "%Y-%m",
+    }
+}
 module.exports = class CarRepository extends GenRepository {
     constructor(){
         super(Car);
     }
-    findCa(groupByValueLimit, groupByType){
-        //unveil here
+
+    generateBaseAggrForGroup(groupByValueLimit,groupByType){
+        return [
+            {
+                $addFields: {
+                    timePeriod: { $dateToString: {format: vars[groupByType].timePeriodFormat, date: "$currentRepair.receptionDate"} },
+                    timePeriodForFilter:  { $dateToString: {format: vars[groupByType].timePeriodFormatForFilter, date: "$currentRepair.receptionDate"} },
+                    ended : "$currentRepair.repairs.ended"
+                }
+            },
+            {
+                $project: { "currentRepair": 0 }
+            },
+            {
+                $match: { timePeriodForFilter: {$eq: formatAndTrunc(groupByValueLimit, groupByType)}, currentRepair: {$and: [{$exists: true}, {$ne: null}]}}
+            },
+           
+        ]
+    } 
+    findCurrentCaRepair(groupByValueLimit, groupByType){
         const collection = this.getCollection();
         collection.aggregate([
+            ...this.generateBaseAggrForGroup(groupByValueLimit,groupByType),
             {
-
+                $unwind: "$ended"
+            },
+            {
+                $group: {
+                    "_id": "$timePeriod",
+                    "amount": {$sum: "$ended.price" }
+                }
             }
         ])
     }
+    findCurrentCarNumberRepair(groupByValueLimit, groupByType){
+        const collection = this.getCollection();
+        collection.aggregate([
+            ...this.generateBaseAggrForGroup(groupByValueLimit,groupByType),
+            {
+                $group: {
+                    "_id": "$timePeriod",
+                    "amount": {$sum: 1 }
+                }
+            }
+        ])
+    }
+  
+
 }
