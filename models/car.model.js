cd7ec068ec05d5a4dbac9a804ca4b466ba15454c
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-01-23 18:49:58 +0300

    add userId to car

--- models/car.model.js
+++ models/car.model.js
@@ -7,10 +7,12 @@ const _id = {
 }
 class Car {
     static schema ={
+        "userId": {
+            type: 'string'
+        },
         "brand": { type: 'string', validatorGetter: (paramPropertyName='brand')=> body(paramPropertyName).isString().withMessage("Marque invalide")  },
         "numberPlate": { type: 'string', validatorGetter: (paramPropertyName='numberPlate')=> body(paramPropertyName).isString().withMessage("Immatriculation invalide")  },
         "description": { type: 'string', validatorGetter: (paramPropertyName='description')=> body(paramPropertyName).isString().withMessage("Description invalide")  },
-        //"weight": { type: 'int',  validatorGetter: (paramPropertyName='weight')=> body(paramPropertyName).isInt().withMessage("Poids invalide").toInt() },
         "status": { type: 'int',  validatorGetter: (paramPropertyName='status')=> body(paramPropertyName).isInt().withMessage("Statut invalide").toInt() },
         "registrationDate": { type: 'date', validatorGetter: (paramPropertyName='registrationDate')=> body(paramPropertyName).isISO8601().withMessage("Date d'enregistrement invalide").toDate() },
         "currentRepair": { classConstructor: Repair},
