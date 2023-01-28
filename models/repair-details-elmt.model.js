a1fee425be19f61af723b78c5af2ef83eb00f871
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-01-28 23:29:21 +0100

    add price constraint

--- models/repair-details-elmt.model.js
+++ models/repair-details-elmt.model.js
@@ -1,10 +1,10 @@
 const {body} = require('express-validator'); 
 class RepairDetailsElmt {
     static schema ={
-            "label" : { type: 'string',  validatorGetter: (paramPropertyName='label')=> body(paramPropertyName).isString().withMessage("Label d'elements de reparations invalides")},
+            "label" : { type: 'string',  validatorGetter: (paramPropertyName='label')=> body(paramPropertyName).isString().notEmpty().withMessage("Label d'elements de reparations invalides")},
             "description": { type: 'string',  validatorGetter: (paramPropertyName='description')=> body(paramPropertyName).isString().withMessage("Description d'elements de reparations invalides")},
-            "price": { type: 'string',  validatorGetter: (paramPropertyName='price')=> body(paramPropertyName).isFloat().withMessage("Prix d'elements de reparations invalide").toFloat()},
-            "progression": { type: 'string',  validatorGetter: (paramPropertyName='progression')=> body(paramPropertyName).isInt().withMessage("Progression invalide").toInt()},
+            "price": { type: 'string',  validatorGetter: (paramPropertyName='price')=> body(paramPropertyName).isFloat({ min:1}).withMessage("Prix d'elements de reparations invalide").toFloat()},
+            "progression": { type: 'string',  validatorGetter: (paramPropertyName='progression')=> body(paramPropertyName).isInt({ min:0, max:100}).withMessage("Progression invalide").toInt()},
         }
     static createSchemaDto = {...this.schema};
     static updateSchemaDto = {...this.schema};
