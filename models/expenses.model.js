aa5e76b109cdcb47227a0a3143f1a2d555b361b7
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-01-29 19:57:48 +0100

    remove time

--- models/expenses.model.js
+++ models/expenses.model.js
@@ -6,7 +6,6 @@ const _id = {
 class Expenses {
     static schema ={
         "expensesDate": { type: 'date', validatorGetter: (paramPropertyName='expensesDate')=> body(paramPropertyName).isISO8601().withMessage("Date invalide").toDate() },
-        "expensesTime": { type: 'time', validatorGetter: (paramPropertyName='expensesTime')=> body(paramPropertyName).isISO8601().withMessage("Heure invalide").toDate() },
         "details": { type: 'array',  validatorGetter: (paramPropertyName='details')=> body(paramPropertyName).isArray().withMessage("Les détails pour les dépenses ne sont pas valide").toArray() },
     }
     static createSchemaDto = {...this.schema};
