d4dfba6fb0fe4321690203ce664bef1655b15037
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-01-29 11:33:48 +0100

    fix general id management

--- commons/database/methods/gen-reflect.js
+++ commons/database/methods/gen-reflect.js
@@ -12,11 +12,13 @@ function assign(targetClass, obj, schemaName='schema'){
         Object.keys(targetClass[schemaName]).map(key=>{ 
             if(targetClass[schemaName][key].classConstructor){
                 newInstance[key] = assign(targetClass[schemaName][key].classConstructor,element[key], schemaName )
-                if(!newInstance[key]._id) newInstance[key]._id = ObjectId();
+                if( !newInstance[key]._id) newInstance[key]._id = ObjectId();
+                if(typeof newInstance._id === 'string') newInstance[key]._id = new ObjectId(newInstance[key]._id)
             }else if(element[key] !== undefined)
                 newInstance[key] = element[key]
         })
         if(!newInstance._id) newInstance._id = ObjectId();
+        if(typeof newInstance._id === 'string') newInstance._id = new ObjectId(newInstance._id)
         newInstances.push(newInstance);
     });
     if(isArray)
